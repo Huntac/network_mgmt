2e353ac302fad0d2c0f669d709d7752927bf41ba
Huntac <ch360409@gmail.com>
2021-09-29 17:41:15 -0400

    docs in agent and action scaling
    Added documentation to ddpg_agent in addition to implementing scaling of ActorNetwork output within the agent class.

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -30,6 +30,7 @@ class Agent:
         self.batch_size = batch_size
         self.max_action = env.action_space.high[0]
         self.min_action = env.action_space.low[0]
+        self.action_range = self.max_action - self.min_action
         self.n_actions = n_actions
         self.nosie = noise
 
@@ -44,25 +45,30 @@ class Agent:
         self.critic_local.compile(optimizer = Adam(learning_rate = beta))
 
         # optimizer only passed so that these models compile
-        # the weights of these networks will be updated via Agent.soft_update
+        # the weights of these networks will be updated via Agent.update_network_parameters
         self.actor_target.compile(optimizer = Adam(learning_rate = alpha))
         self.critic_target.compile(optimizer = Adam(learning_rate = beta))
 
+        # Hard copy initial weights from local networks to target networks
+        #   so the networks are identical
         self.update_network_parameters(tau  = 1)
 
     def update_network_parameters(self, tau = None):
         if tau is None:
+            # Allow to hard copy weights when agent is initialized
             tau = self.tau
 
         weights = []
         targets = self.actor_target.weights
         for i, weight in enumerate(self.actor_local.weights):
+            # apply soft updates from local network -> target network
             weights.append(weight * tau + targets[i]*(1-tau))
         self.actor_target.set_weights(weights)
 
         weights = []
         targets = self.critic_target.weights
         for i, weight in enumerate(self.critic_local.weights):
+            # apply soft updates from local network -> target network
             weights.append(weight * tau + targets[i]*(1-tau))
         self.critic_target.set_weights(weights)
 
@@ -84,38 +90,48 @@ class Agent:
         state = tf.convert_to_tensor([observation], dtype = tf.float32)
         actions = self.actor_local(state)
         if not evaluate:
+            # if not evaluating add noise for exploration
             actions += tf.random.normal(
                 shape=[self.n_actions], 
                 mean = 0, 
                 stddev = self.nosie)
 
-        # still need to scale sigmoid by min_action and max_action
+        # scale sigmoid value with action range
+        actions = tf.math.multiply(actions, self.action_range)
 
+        # clip scaled action to be within environment action range
         actions = tf.clip_by_value(actions, self.min_action, self.max_action)
 
         return actions.numpy()
 
     def learn(self):
         if len(self.memory) < self.batch_size:
+            # Only learn once enough memories have been stored
             return
 
         experiences = self.memory.sample()
 
+        # Unpack experience components
         states = np.vstack(e.state for e in experiences if e is not None)
         actions = np.vstack(e.action for e in experiences if e is not None)
         rewards = np.vstack(e.reward for e in experiences if e is not None)
         dones = np.vstack(e.done for e in experiences if e is not None)
         next_states = np.vstack(e.next_state for e in experiences if e is not None)
 
+        # conver experiences to tensor
         states = tf.convert_to_tensor(states, dtype = tf.float32)
         next_states = tf.convert_to_tensor(next_states, dtype = tf.float32)
         actions = tf.convert_to_tensor(actions, dtype = tf.float32)
         rewards = tf.convert_to_tensor(rewards, dtype = tf.float32)
 
+        # Train 
         with tf.GradientTape() as tape:
             next_actions = self.actor_target(next_states)
             next_values = tf.squeeze(self.critic_target(next_states, next_actions), 1)
             values = tf.squeeze(self.critic_local(states, actions), 1)
+
+            # Target Q value is the observed reward plus the discounted reward of the 
+            #   following time step unless the reward is from a terminal step
             target = reward + self.gamma*next_values*(1-dones)
             critic_loss = keras.losses.MSE(target, values)
 
@@ -127,15 +143,22 @@ class Agent:
             zip(critic_network_gradient, self.critic_local.trainable_variables)
         )
 
+        # Update Actor weights based on critic_local assessment of actor_local policy. 
         with tf.GradientTape() as tape:
             new_policy_actions = self.actor_local(states)
+            # Taking the negative critic_local Q value to do gradient ascent
             actor_loss = -self.critic_local(states, new_policy_actions)
             actor_loss = tf.math.reduce_mean(actor_loss)
 
+        # Because we're passing actor_local predictions into critic_local as input 
+        #   tf.GradientTape() allows us to calculate the gradient of actor_local's
+        #   trainable variable w.r.t the negative Q value from critic_local
         actor_network_gradient = tape.gradient(actor_loss,
         self.actor_local.trainable_variables)
 
         self.actor_local.optimizer.apply_gradient(
             zip(actor_network_gradient, self.actor_local.trainable_variables)
         )
+
+        self.update_network_parameters()
         
